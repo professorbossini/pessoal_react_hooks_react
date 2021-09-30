1b838e1dcb3edf1f3c5136786e3c4b90ffe83cd0
Rodrigo Bossini <professorbossini@gmail.com>
2021-09-30 18:38:04 -0300

    started creating the acoordion component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import Accordion from './Accordion'
+import './Accordion.css'
+const itens = [
+    {
+        titulo: "Java",
+        conteudo: "Linguagem compilada e interpretada."
+    },
+    {
+        titulo: "Python",
+        conteudo: "Linguagem interpretada e dinamicamente tipada."
+    },
+    {
+        titulo: "Javascript",
+        conteudo: "Interpretada. Executa do lado do cliente e do lado do servidor também."
+    }
+]
 const App = () => {
     return (
         <div>
-            Modelo PrimeReact/PrimeFlex
+            <Accordion itens={itens} />
         </div>
     )
 }
