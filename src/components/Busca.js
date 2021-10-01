8d793e8d8a7b6c52044dcedf7f7821c4c5b3b276
Rodrigo Bossini <professorbossini@gmail.com>
2021-10-01 18:11:28 -0300

    Busca tem estado e alguns exemplos com useEffect. Ainda nao faz requisicoes externas

--- src/components/Busca.js
+++ src/components/Busca.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { InputText } from 'primereact/inputtext'
 const Busca = () => {
+    const [termoDeBusca, setTermoDeBusca] = useState('')
+    useEffect (() => {
+        console.log ("executando todas as vezes...")
+    })
+    useEffect(() => {
+        console.log("executando somente uma vez")
+    }, [])
+    useEffect(() => {
+        console.log("Executando somente quando o termo é alterado")
+    }, [termoDeBusca])
     return (
         <div>
+            <span className="p-input-icon-left">
+                <i className="pi pi-search"></i>
+                <InputText 
+                    onChange={(e) => setTermoDeBusca(e.target.value)}
+                />
+            </span>
             
         </div>
     )
