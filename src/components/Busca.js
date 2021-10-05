dc8836bb5a6087721e3beaf299c2553930b08a39
Rodrigo Bossini <professorbossini@gmail.com>
2021-10-05 17:17:31 -0300

    Componente de Busca usando Hooks e um timeout para evitar multiplas requisicoes

--- src/components/Busca.js
+++ src/components/Busca.js
@@ -1,16 +1,45 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { InputText } from 'primereact/inputtext'
+import striptags from 'striptags'
+import { Button } from 'primereact/button'
 const Busca = () => {
-    const [termoDeBusca, setTermoDeBusca] = useState('')
-    useEffect (() => {
-        console.log ("executando todas as vezes...")
-    })
+    const [termoDeBusca, setTermoDeBusca] = useState('React')
+    const [resultados, setResultados] = useState([])
     useEffect(() => {
-        console.log("executando somente uma vez")
-    }, [])
-    useEffect(() => {
-        console.log("Executando somente quando o termo é alterado")
+        //define a função
+        const fazBusca = async () => {
+            const { data } = await axios.get(
+                'https://en.wikipedia.org/w/api.php',{
+                    params: {
+                        action: 'query',
+                        list: 'search',
+                        format: 'json',
+                        // instruindo o navegador a permitir
+                        // conteúdo de qualque origem
+                        origin: '*',
+                        srsearch: termoDeBusca
+                    }   
+                }
+            )
+            setResultados(data.query.search)
+        }
+
+        if (termoDeBusca && !resultados.length){
+            fazBusca()
+        }
+        else{
+            const timeoutID = setTimeout(() => {
+                //execução condicional de novo
+                if (termoDeBusca)
+                    fazBusca()
+            }, 1000)
+            return () => {
+                clearTimeout(timeoutID)
+            }
+        }
     }, [termoDeBusca])
+
     return (
         <div>
             <span className="p-input-icon-left">
@@ -19,7 +48,28 @@ const Busca = () => {
                     onChange={(e) => setTermoDeBusca(e.target.value)}
                 />
             </span>
-            
+            {
+                resultados.map((resultado) => (
+                    <div 
+                        key={resultado.pageid}
+                        className="my-2 border border-1 border-400">
+                        <div 
+                            className="border-bottom border border-1 border-400 p-2 text-center font-bold">
+                                {resultado.title}
+                                <span>
+                                <Button 
+                                    icon="pi pi-send" 
+                                    className=" ml-2 p-button-rounded p-button-secondary" 
+                                    onClick={() => window.open(`https://en.wikipedia.org?curid=${resultado.pageid}`)}
+                                    />
+                                </span>
+                        </div>
+                        <div className="p-2">
+                            <span dangerouslySetInnerHTML={{__html: resultado.snippet}}></span>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
+
 import 'primereact/resources/primereact.min.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
